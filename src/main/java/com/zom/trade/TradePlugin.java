5f80f53b25cbf21bb5a98edf1579d31db16252b1
redrumze <null>
2021-04-23 14:16:18 -0400

    Format large numbers to include commas & prevent spam of errors of substring

--- src/main/java/com/zom/trade/TradePlugin.java
+++ src/main/java/com/zom/trade/TradePlugin.java
@@ -1,5 +1,7 @@
 package com.zom.trade;
 
+import java.text.NumberFormat;
+import java.util.Locale;
 import javax.inject.Inject;
 import net.runelite.api.Client;
 import net.runelite.api.Item;
@@ -51,21 +53,26 @@ public class TradePlugin extends Plugin
 	@Subscribe
 	public void onClientTick(ClientTick clientTick)
 	{
-		// 3 is left trade screen (yours) | 4 is right trade screen (theirs)
-		if (client.getWidget(WidgetID.PLAYER_TRADE_SCREEN_GROUP_ID, 3) != null && client.getWidget(WidgetID.PLAYER_TRADE_SCREEN_GROUP_ID, 4) != null)
+		try
 		{
-			firstTradeScreen();
-		}
+			// 3 is left trade screen (yours) | 4 is right trade screen (theirs)
+			if (client.getWidget(WidgetID.PLAYER_TRADE_SCREEN_GROUP_ID, 3) != null && client.getWidget(WidgetID.PLAYER_TRADE_SCREEN_GROUP_ID, 4) != null)
+			{
+				firstTradeScreen();
+			}
 
-		// 334 is the second trade screen, 4 is the child for it.
-		if (client.getWidget(334, 4) != null)
-		{
-			secondTradeScreen();
+			// 334 is the second trade screen, 4 is the child for it.
+			if (client.getWidget(334, 4) != null)
+			{
+				secondTradeScreen();
+			}
+		} catch (Exception e) {
+			// ignored to not spam logs
 		}
 	}
 
 	// do first trade screen logic
-	public void firstTradeScreen()
+	public void firstTradeScreen() throws Exception
 	{
 		myItemPrice = 0;
 		theirItemsPrice = 0;
@@ -120,10 +127,10 @@ public class TradePlugin extends Plugin
 		{
 			theirColor = IMBALANCED_TRADE;
 		}
-		String myTradeText = "You offer:<br>(Value: <col=ffffff>" + (myItemPrice == 1 ? "One" : myItemPrice) + "</col> " + (myItemPrice == 1 ? "coin" : "coins") + ")";
+		String myTradeText = "You offer:<br>(Value: <col=ffffff>" + (myItemPrice == 1 ? "One" : format(myItemPrice)) + "</col> " + (myItemPrice == 1 ? "coin" : "coins") + ")";
 
 		String theirName = theirTradeOffer.getText().substring(0, theirTradeOffer.getText().indexOf(" offers:<br>"));
-		String theirTradeText = theirName + " offers:<br>(Value: <col=ffffff>" + (theirItemsPrice == 1 ? "One" : theirItemsPrice) + "</col> " + (theirItemsPrice == 1 ? "coin" : "coins") + ")";
+		String theirTradeText = theirName + " offers:<br>(Value: <col=ffffff>" + (theirItemsPrice == 1 ? "One" : format(theirItemsPrice)) + "</col> " + (theirItemsPrice == 1 ? "coin" : "coins") + ")";
 
 		myTradeOffer.setText(myTradeText);
 		theirTradeOffer.setText(theirTradeText);
@@ -131,7 +138,7 @@ public class TradePlugin extends Plugin
 	}
 
 	// do second screen stuff
-	public void secondTradeScreen()
+	public void secondTradeScreen() throws Exception
 	{
 
 		Widget theirSecondTradeText = client.getWidget(334, 24);
@@ -146,10 +153,14 @@ public class TradePlugin extends Plugin
 			return;
 		}
 
-		String myTradeText = "You are about to give:<br>(Value: <col=ffffff>" + (myItemPrice == 1 ? "One" : myItemPrice) + "</col> " + (myItemPrice == 1 ? "coin" : "coins") + ")";
-		String theirTradeText = "In return you will receive:<br>(Value: <col=" + (myItemPrice > theirItemsPrice ? RED_COLOR : WHITE_COLOR) + ">" + (theirItemsPrice == 1 ? "One" : theirItemsPrice) + "</col> " + (theirItemsPrice == 1 ? "coin" : "coins") + ")";
+		String myTradeText = "You are about to give:<br>(Value: <col=ffffff>" + (myItemPrice == 1 ? "One" : format(myItemPrice)) + "</col> " + (myItemPrice == 1 ? "coin" : "coins") + ")";
+		String theirTradeText = "In return you will receive:<br>(Value: <col=" + (myItemPrice > theirItemsPrice ? RED_COLOR : WHITE_COLOR) + ">" + (theirItemsPrice == 1 ? "One" : format(theirItemsPrice)) + "</col> " + (theirItemsPrice == 1 ? "coin" : "coins") + ")";
 
 		mySecondTradeText.setText(myTradeText);
 		theirSecondTradeText.setText(theirTradeText);
+	}
+
+	public String format(long number) {
+		return NumberFormat.getNumberInstance(Locale.UK).format(number);
 	}
 }
