67997daca9bb8c433d290397ab4dbc0b0bcb479d
zguilt <redzguilt@gmail.com>
2023-08-25 19:17:54 -0400

    jzom updates
    use Instant
    use config group for onConfigChanged
    format file consistently

--- src/main/java/com/rc_lap_tracker/RCLapTrackerPlugin.java
+++ src/main/java/com/rc_lap_tracker/RCLapTrackerPlugin.java
@@ -1,34 +1,33 @@
 package com.rc_lap_tracker;
 
-import javax.inject.Inject;
-
 import com.google.inject.Provides;
+import static com.rc_lap_tracker.RCLapTrackerConfig.GROUP_NAME;
+import java.awt.image.BufferedImage;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.*;
+import net.runelite.api.Client;
+import net.runelite.api.InventoryID;
+import net.runelite.api.ItemID;
+import net.runelite.api.events.AnimationChanged;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.client.callback.ClientThread;
+import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.events.ConfigChanged;
+import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
-import net.runelite.client.task.Schedule;
-import net.runelite.client.ui.overlay.infobox.Counter;
-import net.runelite.client.config.ConfigManager;
-
-import java.awt.image.BufferedImage;
-import net.runelite.api.Client;
-import net.runelite.client.callback.ClientThread;
-import net.runelite.client.game.ItemManager;
 import net.runelite.client.ui.overlay.infobox.InfoBoxManager;
-
-import net.runelite.api.events.AnimationChanged;
 
 
 @Slf4j
 @PluginDescriptor(
-		name = "Runecraft Lap Tracker",
-		description = "Shows you how many laps until your largest pouch degrades",
-		tags = {"runecraft", "rune", "lap", "tracker", "pouch", "essence", "zmi"}
+	name = "Runecraft Lap Tracker",
+	description = "Shows you how many laps until your largest pouch degrades",
+	tags = {"runecraft", "rune", "lap", "tracker", "pouch", "essence", "zmi"}
 )
 public class RCLapTrackerPlugin extends Plugin
 {
@@ -52,68 +51,80 @@ public class RCLapTrackerPlugin extends Plugin
 	{
 		return configManager.getConfig(RCLapTrackerConfig.class);
 	}
+
 	private jwowWriteableCounter counterBox;
 
-	private long lastAction;
+	// variables to control drawing info boxes
+	private Instant lastAction;
+	private boolean isActive;
+
 	private int target;
 	private int cycle;
 	private boolean hasCrafted;
 	private boolean isMidRun;
 
-	private boolean isActive;
 	private static final int SPELL_CONTACT_ANIMATION_ID = 4413;
 	private static final int CRAFT_RUNES_ANIMATION_ID = 791;
 
-	private int getIntConfig(String key){
-		Integer value = configManager.getRSProfileConfiguration(RCLapTrackerConfig.GROUP_NAME, key, int.class);
+	private int getIntConfig(String key)
+	{
+		Integer value = configManager.getRSProfileConfiguration(GROUP_NAME, key, int.class);
 		return value == null ? -1 : value;
 	}
 
-	private boolean getBooleanConfig(String key){
-		Boolean value = configManager.getRSProfileConfiguration(RCLapTrackerConfig.GROUP_NAME, key, boolean.class);
+	private boolean getBooleanConfig(String key)
+	{
+		Boolean value = configManager.getRSProfileConfiguration(GROUP_NAME, key, boolean.class);
 		return value != null && value;
 	}
 
-	private void setConfig(String key, Object value){
-		configManager.setRSProfileConfiguration(RCLapTrackerConfig.GROUP_NAME, key, value);
+	private void setConfig(String key, Object value)
+	{
+		configManager.setRSProfileConfiguration(GROUP_NAME, key, value);
 	}
 
 	@Override
-	protected void startUp(){
+	protected void startUp()
+	{
 		target = config.highestPouch().getTarget();
 		cycle = getIntConfig(RCLapTrackerConfig.CYCLE_KEY);
 		if (cycle == -1)
+		{
 			cycle = target;
+		}
 
 		hasCrafted = getBooleanConfig(RCLapTrackerConfig.HASCRAFTED_KEY);
 		isMidRun = getBooleanConfig(RCLapTrackerConfig.ISMIDRUN_KEY);
 		counterBox = null;
-		lastAction = System.currentTimeMillis();
 
 		updateInfoBox();
 	}
+
 	@Subscribe
 	public void onGameTick(GameTick event)
 	{
-		boolean wasActive = isActive;
-
-		if (System.currentTimeMillis() - lastAction < 300000)
-			isActive = true;
-		else
-			isActive = false;
-
-		if (wasActive != isActive)
-			updateInfoBox();
+		if (lastAction != null)
+		{
+			if (lastAction.plus(300, ChronoUnit.SECONDS).isBefore(Instant.now()))
+			{
+				isActive = false;
+			}
+		}
+		updateInfoBox();
 	}
 
 	@Subscribe
 	public void onConfigChanged(ConfigChanged ev)
 	{
-		int oldTarget = target;
-		target = config.highestPouch().getTarget();
-		if (oldTarget != target && !isMidRun) {
-			cycle = target;
-			updateInfoBox();
+		if (ev.getGroup().equals(GROUP_NAME))
+		{
+			int oldTarget = target;
+			target = config.highestPouch().getTarget();
+			if (oldTarget != target && !isMidRun)
+			{
+				cycle = target;
+				updateInfoBox();
+			}
 		}
 	}
 
@@ -129,7 +140,8 @@ public class RCLapTrackerPlugin extends Plugin
 
 	private void removeInfobox()
 	{
-		if (counterBox != null) {
+		if (counterBox != null)
+		{
 			infoBoxManager.removeInfoBox(counterBox);
 		}
 		counterBox = null;
@@ -164,22 +176,28 @@ public class RCLapTrackerPlugin extends Plugin
 		String playerName = client.getLocalPlayer().getName();
 		String actorName = event.getActor().getName();
 
-		if (!playerName.equals(actorName)) {
+		if (!playerName.equals(actorName))
+		{
 			return;
 		}
 
 		int animId = event.getActor().getAnimation();
-		if (animId == SPELL_CONTACT_ANIMATION_ID) {
+		if (animId == SPELL_CONTACT_ANIMATION_ID)
+		{
 			cycle = target;
 			isMidRun = false;
-			lastAction = System.currentTimeMillis();
+			lastAction = Instant.now();
+			isActive = true;
 			updateInfoBox();
 		}
-		else if (!hasCrafted && animId == CRAFT_RUNES_ANIMATION_ID) {
+		else if (!hasCrafted && animId == CRAFT_RUNES_ANIMATION_ID)
+		{
 			hasCrafted = true;
 			isMidRun = true;
-			lastAction = System.currentTimeMillis();
-			if (cycle > 0) {
+			lastAction = Instant.now();
+			isActive = true;
+			if (cycle > 0)
+			{
 				cycle--;
 			}
 			updateInfoBox();
